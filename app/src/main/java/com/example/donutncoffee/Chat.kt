57f11d3887a28c767204354b83e090110551c2ef
matheusgmes <gomesmatheus.eo@gmail.com>
2019-09-12 13:20:12 -0300

    Chat 2.1

---
+++ app/src/main/java/com/example/donutncoffee/Chat.kt
@@ -0,0 +1,17 @@
+package com.example.donutncoffee
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_chat.*
+
+
+class Chat : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+
+
+    }
+}
